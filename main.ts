7b1d17d6e83b0e8af108956e594e70ab4161b127
TrzasQ <przytrzask@gmail.com>
2023-09-03 19:52:52 +0200

    Aktualizacja main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -13,13 +13,11 @@ input.onGesture(Gesture.TiltRight, function () {
 input.onButtonPressed(Button.A, function () {
     music.play(music.tonePlayable(147, music.beat(BeatFraction.Double)), music.PlaybackMode.UntilDone)
 })
-input.onButtonPressed(Button.B, function () {
-    if (wlaczony) {
-        SuperBit.MotorRun(SuperBit.enMotors.M1, 0)
-        wlaczony = 0
+radio.onReceivedValue(function (name, value) {
+    if (name == "skręt") {
+        SuperBit.Servo(SuperBit.enServo.S2, value)
     } else {
-        SuperBit.MotorRun(SuperBit.enMotors.M1, 255)
-        wlaczony = 1
+        SuperBit.MotorRun(SuperBit.enMotors.M1, value)
     }
 })
 input.onGesture(Gesture.ThreeG, function () {
@@ -29,10 +27,9 @@ input.onGesture(Gesture.Shake, function () {
     music.play(music.tonePlayable(247, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
 })
 let gaz = 0
-let wlaczony = 0
 let x = 0
+let wlaczony = 0
 radio.setGroup(1)
-wlaczony = 0
 const lerp = (x: number, y: number, a: number) => x * (1 - a) + y * a;
 const invlerp = (x: number, y: number, a: number) => clamp((a - x) / (y - x));
 const clamp = (a: number, min = 0, max = 1) => Math.min(max, Math.max(min, a));
@@ -53,16 +50,10 @@ basic.forever(function () {
     // 0 to 1023
     x = pins.analogReadPin(AnalogPin.P1)
     const xAxis = range(0, 1023, 70, 200, x)
-SuperBit.Servo(SuperBit.enServo.S2, xAxis)
-    radio.sendValue("skręt", xAxis)
+radio.sendValue("skręt", xAxis)
     if (gaz > 4) {
-        SuperBit.MotorRun(SuperBit.enMotors.M1, 0)
         radio.sendValue("gaz", 0)
     } else {
-        SuperBit.MotorRun(SuperBit.enMotors.M1, -255)
         radio.sendValue("gaz", -255)
     }
-})
-basic.forever(function () {
-	
 })
